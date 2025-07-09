098269a2402df6d5ffb44fa5dfde5095bab3c811
Rizqi Andhika <qiewifruit@gmail.com>
2025-07-09 14:16:59 +0700

    feat: article image

--- components/Cards/ArticleCard.vue
+++ components/Cards/ArticleCard.vue
@@ -1,11 +1,12 @@
 <template>
   <article class="group cursor-pointer">
     <div class="mb-6 aspect-video overflow-hidden rounded-lg bg-gray-100">
-      <div
-        class="flex h-full w-full items-center justify-center bg-gradient-to-br from-gray-200 to-gray-300 transition-transform duration-300 group-hover:scale-105"
-      >
-        <span class="text-lg text-gray-500">Article Image</span>
-      </div>
+      <nuxt-img
+        src="/assets/img/article-image.jpg"
+        :alt="article.title"
+        class="h-full w-full object-cover transition-transform duration-300 group-hover:scale-105"
+        loading="lazy"
+      />
     </div>
 
     <div class="mb-3">
